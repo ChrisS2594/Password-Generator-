af85e61ec3e9de5c497f4813e7105b2c4879daad
Chris Stephens <stephens-c2@live.com>
2019-11-16 13:00:41 -0700

    rewriting java script to remove syntax errors on lines 2, 4, and 9

--- javascript.js
+++ javascript.js
@@ -1,19 +1,36 @@
-function specialchar(){
-    alert('do you want to add a special character?');
-    var generate= ["!, @, #, $,"] ;
-    for (i= true, i < password.length; i++;)
-    document.getElementById("password").innerHTML = Math.random();
-}
+let password = document.getElementById("password");
 
-function addNumber(){
-    alert('do you want to add a number?');
-    var specialchar= ["1, 2, 3, 5, 6, 7, 8, 9, 0"] ;
-    for (i= true , i < password.length; i++;)
+let generate = document.getElementById("generate");
+    generate.addEventListener("click", generate())
 
-    document.getElementById("password").innerHTML = Math.random();
-}
+let upperletterarray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-function button(){
-    onClick
-}
+let lowerletterarray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+let unicodearray = ['\u0020', '\u0021', '\u0022', '\u0023', '\u0024', '\u0025', '\u0026', '\u0027','\u0028', '\u0029', '\u002A', '\u002B', '\u002C', '\u002D', '\u002E', '\u002F', '\u003A', '\u003B', '\u003C', '\u003D', '\u003E', '\u003F', '\u0040', '\u005B', '\u005C', '\u005D', '\u005E', '\u005F', '\u0060', '\u007B', '\u007C', '\u007D', '\u007E']
+    
+let integerarray =['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    function generate(upperletterarray , lowerletterarray , unicodearray , integerarray) {
+        return upperletterarray + lowerletterarray + unicodearray + integerarray;
+
+    };
+    console.log("generate");
+
+document.getElementById("password");
+
+
+
+
 
