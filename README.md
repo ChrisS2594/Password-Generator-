f44f9bca8390d9bff51197e5987fde030ca6158c
Chris Stephens <stephens-c2@live.com>
2019-11-15 19:34:01 -0700

    starting README.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Password-Generator-
 Homework 3
+This application is made to generate a random password between 8 and 128 characters. 
+
--- javascript.js
+++ javascript.js
@@ -1,23 +1,18 @@
 function specialchar(){
     alert('do you want to add a special character?');
-    var specialchar= ["!, @, #, $,"] ;
-    if (i= true i < password.length; i++);
-    password[1].setAttribute("");
-    document.getElementById("").innerHTML = Math.random();
+    var generate= ["!, @, #, $,"] ;
+    for (i= true, i < password.length; i++)
+    document.getElementById("password").innerHTML = Math.random();
 }
 
 function addNumber(){
     alert('do you want to add a number?');
     var specialchar= ["1, 2, 3, 5, 6, 7, 8, 9, 0"] ;
-    if (i= true i < password.length; i++);
-   
+    for (i= true , i < password.length; i++);
+
     document.getElementById("password").innerHTML = Math.random();
 }
 
-function specialchar(){
-    alert('do you want to add a special character?');
-    var specialchar= ["!, @, #, $,"] ;
-    if (i= true i < password.length; i++);
-    password[1].setAttribute("")
-    document.getElementById("").innerHTML = Math.random();
+function button(){
+    onClick
 }
