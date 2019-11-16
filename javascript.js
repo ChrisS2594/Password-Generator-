9f9e5cd1557eafe9aa2531e2fe264939b56ea2e4
Chris Stephens <stephens-c2@live.com>
2019-11-15 19:12:40 -0700

    initial commit

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function generatePassword() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
+function specialchar(){
+    alert('do you want to add a special character?');
+    var specialchar= ["!, @, #, $,"] ;
+    if (i < true)
+}
