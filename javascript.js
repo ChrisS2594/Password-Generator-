519465477acb05806242118a4134e7f66d7bab81
Chris Stephens <stephens-c2@live.com>
2019-11-15 20:28:48 -0700

    fixed : coding error on lines 4 and 11

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 function specialchar(){
     alert('do you want to add a special character?');
     var generate= ["!, @, #, $,"] ;
-    for (i= true, i < password.length; i++)
+    for (i= true, i < password.length; i++;)
     document.getElementById("password").innerHTML = Math.random();
 }
 
 function addNumber(){
     alert('do you want to add a number?');
     var specialchar= ["1, 2, 3, 5, 6, 7, 8, 9, 0"] ;
-    for (i= true , i < password.length; i++);
+    for (i= true , i < password.length; i++;)
 
     document.getElementById("password").innerHTML = Math.random();
 }
@@ -16,3 +16,4 @@ function addNumber(){
 function button(){
     onClick
 }
+
