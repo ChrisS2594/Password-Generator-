2dd7dceaa79e6bd6426982aefc8cfe636d8544ae
Chris Stephens <stephens-c2@live.com>
2019-11-15 19:21:15 -0700

    adding JS to commit

--- javascript.js
+++ javascript.js
@@ -1,15 +1,23 @@
-function generatePassword() {
-    var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
+function specialchar(){
+    alert('do you want to add a special character?');
+    var specialchar= ["!, @, #, $,"] ;
+    if (i= true i < password.length; i++);
+    password[1].setAttribute("");
+    document.getElementById("").innerHTML = Math.random();
+}
+
+function addNumber(){
+    alert('do you want to add a number?');
+    var specialchar= ["1, 2, 3, 5, 6, 7, 8, 9, 0"] ;
+    if (i= true i < password.length; i++);
+   
+    document.getElementById("password").innerHTML = Math.random();
 }
 
 function specialchar(){
     alert('do you want to add a special character?');
     var specialchar= ["!, @, #, $,"] ;
-    if (i < true)
+    if (i= true i < password.length; i++);
+    password[1].setAttribute("")
+    document.getElementById("").innerHTML = Math.random();
 }
